60e61e380c573b4d8909d4deb95dd910f8fa13ee
unknown <polanskiy.html@gmail.com>
2020-09-18 23:45:35 +0300

    js

--- js/slick.js
+++ js/slick.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
     
     });
 });
+const body = document.querySelector("body");
+const checkBox = document.getElementById('menu__burger_check')
+const headerMenu = document.querySelector('.header__menu')
+
+if(body && checkBox) {
+  body.addEventListener('click', function() {
+    if(checkBox.checked){
+      console.log('123')
+      checkBox.checked = false;
+    }
+  })
+  headerMenu.addEventListener('click', function(e) {
+    e.stopPropagation()
+  })
+}
